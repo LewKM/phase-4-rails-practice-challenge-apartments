d5a0b7d217640deba38c2051d0d77dbf91a1a46b
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-08 15:17:41 +0300

    Adds the delete feature for the lease model

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -7,9 +7,23 @@ class LeasesController < ApplicationController
         render json: lease, status: :created
     end
 
+    def destroy
+        lease = Lease.find(params[:id])
+        lease.destroy
+        head :no_content
+    end
+
     private 
 
     def lease_params
         params.permit(:rent, :apartment_id, :tenant_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
+
+    def render_not_found_response
+        render json: { error: "Lease not found" }, status: :not_found
     end
 end
