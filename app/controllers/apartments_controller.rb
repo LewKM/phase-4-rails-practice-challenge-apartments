d2c30b1b153c319b2fe633c82068c874a5fcfada
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-08 14:51:04 +0300

    Adds the get operation for the apartment model.

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,2 +1,9 @@
 class ApartmentsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index  
+        apartments = Apartment.all
+        render json: apartments, status: :ok
+    end
 end
