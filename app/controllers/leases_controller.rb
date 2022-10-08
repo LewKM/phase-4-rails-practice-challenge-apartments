ad538f7b13898bb8a39b06e3e360b637ae9783e0
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-08 15:15:39 +0300

    Adds the insert feature for the leases

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,2 +1,15 @@
 class LeasesController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def create
+        lease = Lease.create!(lease_params)
+        render json: lease, status: :created
+    end
+
+    private 
+
+    def lease_params
+        params.permit(:rent, :apartment_id, :tenant_id)
+    end
 end
