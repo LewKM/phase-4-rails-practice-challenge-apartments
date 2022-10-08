4c66572bd91bdc5bf161412ccfb13051849761ae
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-08 14:56:37 +0300

    Adds the create feature for the apartment model

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -12,4 +12,15 @@ class ApartmentsController < ApplicationController
         render json: apartment, status: :ok
     end
 
+    def create
+        apartment = Apartment.create!(apartment_params)
+        render json: apartment, status: :created
+    end
+
+    private
+
+    def apartment_params
+        params.permit(:number)
+    end
+
 end
